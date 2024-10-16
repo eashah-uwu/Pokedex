927b56d5933033be6a6c4b433d6f46eb13d2bff0
eashah-uwu <ebajwa.bscs22seecs@seecs.edu.pk>
2024-10-16 16:19:11 +0500

    signup

---
+++ providers/TanstackProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+const TanstackProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
